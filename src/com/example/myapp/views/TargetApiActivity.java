a007540b765bfdc6ecfc6aec33f1b614508df4d3
ouyangzhouchao <oyzc0001@gmail.com>
2014-06-03 14:34:17 +0800

    init

---
+++ src/com/example/myapp/views/TargetApiActivity.java
@@ -0,0 +1,48 @@
+package com.example.myapp.views;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.example.myapp.R;
+
+import java.util.List;
+
+/**
+ * Created by ouyangzhouchao on 14-6-3.
+ */
+public class TargetApiActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.target_api);
+        Button run = (Button)findViewById(R.id.run);
+        run.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadInfo();
+            }
+        });
+    }
+
+    @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+    private void loadInfo() {
+        long installTime ;
+        PackageManager pm = this.getPackageManager();
+        List<PackageInfo> packages = pm.getInstalledPackages(0);
+        PackageInfo packageInfo = packages.get(0);
+        if (pm.getLaunchIntentForPackage(packageInfo.packageName) != null) {
+            try {
+                installTime = packageInfo.firstInstallTime;
+            } catch (Exception e) {
+                installTime = 0;
+            }
+        }
+    }
+}
