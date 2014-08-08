ad02493c2765b70ca3110297cad68b776100acd1
ouyangzhouchao <oyzc0001@gmail.com>
2014-08-08 18:02:46 +0800

    modify

---
+++ src/com/example/myapp/activity/ActionBarActivity.java
@@ -0,0 +1,10 @@
+package com.example.myapp.activity;
+
+import android.app.Activity;
+
+/**
+ * Created by ouyangzhouchao on 7/24/14.
+ */
+public class ActionBarActivity extends Activity {
+
+}
---
+++ src/com/example/myapp/activity/BaiduLocationActivity.java
@@ -0,0 +1,9 @@
+package com.example.myapp.activity;
+
+import android.app.Activity;
+
+/**
+ * Created by ouyangzhouchao on 7/25/14.
+ */
+public class BaiduLocationActivity extends Activity {
+}
